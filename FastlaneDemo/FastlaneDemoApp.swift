7b1b5ad19883250718b09cd9b45e07915c4579e4
iTBoyer <iTBoyer@qq.com>
2022-10-21 09:03:13 +0800

    Initial Commit

---
+++ FastlaneDemo/FastlaneDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastlaneDemoApp.swift
+//  FastlaneDemo
+//
+//  Created by boyer on 2022/10/21.
+//
+
+import SwiftUI
+
+@main
+struct FastlaneDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
